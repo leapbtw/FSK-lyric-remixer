6c9434cbbea388b5a445b530b9e9f558a5cfe41c
leapbtw <36914991+leapbtw@users.noreply.github.com>
2020-09-11 15:36:42 +0200

    + POLKA

--- fsk-remixer/main.cpp
+++ fsk-remixer/main.cpp
@@ -12,15 +12,18 @@ using namespace std;
 const int intrit1 = 10;
 string ritornellop1[intrit1] = {"e sai che ho detto '", "negro siamo ", "catene ", "ma non con il ", "solo toc-toc, ", "penso alla ", "la bagno come ", "Ta-ta-taxi e FSK uguale a ", "E dentro il bomber di ",
     "Uno, due, tre, "};
-const int intrit2 = 10;
-string ritornellop2[intrit2] = {" stop'", " pura", ", non credo in Jesus", " levo la vita agli infami", " in pigggiama", " tu che parli?", ", si", " in Italia", " che fa più luce il polo nord", " stop"};
+const int intrit2 = 11;
+string ritornellop2[intrit2] = {" stop'", " pura", ", non credo in Jesus", " levo la vita agli infami", " in pigggiama", " tu che parli?", ", si", " in Italia", " che fa più luce il polo nord", " stop", " cocaleros"};
 
-const int intversipre = 3;
-string versipre[intversipre] = {" nella Cuki", " Vanno pazzi per il cuoco", " sono benzodiazapina, ehi"};
-const int intversiIntegri = 6;
-string versiIntegri[intversiIntegri] = {"Non fotterò piu' con quella merda di Xanax", "No, tu non sei come me, ha", "Da noi è sempre mezzogiorno", "Sapo in tasca ha una pistola (attento)", "Sta piovendo cash (ok)"};
-const int intversipost = 5;
-string versipost[intversipost] = {"siamo nel Club con dieci ", "Sapo in tasca ha ", "Ciabattine Armani, Taxi B, Gucci e ", "Spostavamo pacchi, ora alziamo ", "Sai che lo faccio col gang, mica lo faccio con "};
+const int intversipre = 7;
+string versipre[intversipre] = {" nella Cuki", " Vanno pazzi per il cuoco", " sono benzodiazepina, ehi", " freddo con il denim", " sei solo uno snitch, te lo leggo in faccia", " e' come un frontale contro un muro", " non sono frocio, lo amo davvero"};
+const int intversiIntegri = 11;
+string versiIntegri[intversiIntegri] = {"Non fotterò piu' con quella merda di Xanax", "No, tu non sei come me, ha", "Da noi è sempre mezzogiorno", "Sapo in tasca ha una pistola (attento)", "Sta piovendo cash (ok)", "Co-coca-cola, sembra il Monte Everest", "Polka-a-a-a-a, bro, ah", "Erba nel culo, vengo da Amsterdam", "Sembra che ho il cazzo lungo un metro", "Big Mac, mangio tre chopper", "Ballo la polka, faccio tre mosse"};
+const int intversipost = 8;
+string versipost[intversipost] = {"siamo nel Club con dieci ", "Sapo in tasca ha ", "Ciabattine Armani, Taxi B, Gucci e ", "Spostavamo pacchi, ora alziamo ", "Sai che lo faccio col gang, mica lo faccio con ", "Scrivo cose sopra i treni, sembro ", "Sexboy, sopra al polka sembro ", "Greg Willen mi fa sentire "};
+
+const int intecho = 12;
+string echo[intecho] = {" (attento)", " (ok)", " (rosa)", " (denim)", " (swag)", " (c'ho paura)", " (sexy)", " (tu-tu-tu)", " (come un'escort)", " (lunghissimo)", " (cago l'Olanda)", " (snitch, snitch)"};
 
 //done
 const int intnomi = 20;
@@ -39,7 +42,7 @@ int main() {
     
     //verso 1
     bool gangbang = true;
-    if ((rand()%10)< 4) gangbang = false; //rand di qualcosa non mi ricordo
+    if ((rand()%10)< 4) gangbang = false; //rand se la frase finisce con GANG e BANG
     cout << nomi[rand()%intnomi] << versipre[rand()%intversipre] << endl;
     cout << versipost[rand()%intversipost] << nomi[rand()%intnomi] << endl;
     cout <<ritornellop1[rand()%intrit1] << nomi[rand()%intnomi] << ritornellop2[rand()%intrit2] << endl;
@@ -47,7 +50,7 @@ int main() {
     if (gangbang == false) cout << " BANG!" << endl;
     else cout << endl;
     cout << nomi[rand()%intnomi] << versipre[rand()%intversipre];
-    if (gangbang == false) cout << ", gang!" << endl;
+    if (gangbang == false) cout << ", GANG!" << endl;
     else cout << endl;
     
     //ritornello
@@ -59,18 +62,18 @@ int main() {
     int nomiN = rand()%intnomi;
     cout << endl << versiIntegri[versiIntegro1] << endl;
     if ((rand()%10) < 5) {
-        cout << ritornellop1[ritornelloN] << nomi[nomiN] << ritornellop2[ritornelloN2] << endl;
+        cout << ritornellop1[ritornelloN] << nomi[nomiN] << ritornellop2[ritornelloN2] << echo[rand()%intecho] << endl;
     }
     else {
-        cout << versi[rand()%intversi] << cout;
+        cout << versi[rand()%intversi] << echo[rand()%intecho] << endl;
     }
     cout << versiIntegri[rand()%intversiIntegri] << endl;
-    cout << ritornellop1[ritornelloN] << nomi[nomiN] << ritornellop2[ritornelloN2] << endl;
+    cout << ritornellop1[ritornelloN] << nomi[nomiN] << ritornellop2[ritornelloN2] << echo[rand()%intecho] << endl;
     cout << versiIntegri[versiIntegro1] << endl;
-    cout << ritornellop1[ritornelloN3] << nomi[nomiN] << ritornellop2[ritornelloN4] << endl;
+    cout << ritornellop1[ritornelloN3] << nomi[nomiN] << ritornellop2[ritornelloN4] << echo[rand()%intecho] << endl;
     
     
-    cout << endl << endl;
+    cout << endl;
     //verso 2
     cout << nomi[rand()%intnomi] << versipre[rand()%intversipre] << endl;
     cout << writers[rand()%intwriters] << writers[rand()%intwriters] << endl;
@@ -90,7 +93,7 @@ int main() {
     cout << ritornellop1[ritornelloN3] << nomi[nomiN] << ritornellop2[ritornelloN4] << endl;
     
     //
-    cout << endl << endl;
+    cout << endl;
     //pre ritornello
     cout << nomi[rand()%intnomi] << versipre[rand()%intversipre] << endl;
     cout << versipost[rand()%intversipost] << nomi[rand()%intnomi] << endl;
